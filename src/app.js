50c658a6cd18f54f060f84ff430c3e865a275a20
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2022-09-22 09:36:18 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
 const app = express();
-const routes = require('./routes/main')
+const routes = require('./routes/main');
+const Detail = require('./models/Detail');
 const mongoose = require('mongoose');
 
 app.use("",routes);
@@ -11,10 +12,37 @@ app.use('/static',express.static('public'));
 //templating engine
 app.set('view engine', 'hbs');
 app.set('views','views');
+hbs.registerPartials('views/partials');
 
 //db connection
-mongoose.connect('mongodb://localhost/website_tut',()=>{
+mongoose.connect('mongodb+srv://itswaleedtahir:itswaleedtahir123@cluster0.smf6v.mongodb.net/website_tut',()=>{
     console.log('Database connected');
+    // Detail.create({
+    //     brandName: "Waleedz Photography",
+    //     brandIconUrl:"https://yt3.ggpht.com/uBceQFYCeTQLHxcq8J1Ok9cT0HStGyr5t5UkXa8APs-rbysfBtpOMyLzBvixm5hkiFfYo5CvO90=s88-c-k-c0x00ffffff-no-rj",
+    //     links:[
+    //         {
+    //             label: 'Home',
+    //             url: '/'
+    //         },
+    //         {
+    //             label:"Services",
+    //             url:"/services"
+    //         },
+    //         {
+    //             label: 'Gallery',
+    //             url:'/gallery'
+    //         },
+    //         {
+    //             label:'About',
+    //             url:'/about'
+    //         },
+    //         {
+    //             label:'Contact',
+    //             url:'/contact-us'
+    //         },
+    //     ]
+    // })
 })
 
 app.listen(process.env.PORT | 8000, ()=>{
