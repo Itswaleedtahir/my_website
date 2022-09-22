c3f0ef792a3ed9d9f62d696cf26a6f5ed69d103a
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2022-09-22 13:13:58 +0500

    Update main.js

--- src/routes/main.js
+++ src/routes/main.js
@@ -3,15 +3,18 @@ const express = require("express");
 const routes = express();
 const Detail = require('../models/Detail');
 const Slider = require('../models/Slider');
+const Service = require('../models/Service');
 
 routes.get('/', async (req,res)=>{
 
     const details = await Detail.findOne({"_id":"632bdfac85cf55ca4dfcccf0"});
     const slides = await Slider.find();
+    const services = await Service.find();
   //  console.log(slides);
    res.render('index',{
         details: details,
         slides: slides,
+        services: services
    })
 });
 
