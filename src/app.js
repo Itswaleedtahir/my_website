801bc3ed8e878571e3f17a1f9ed06f044a762182
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2022-09-22 11:30:50 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const hbs = require('hbs');
 const app = express();
 const routes = require('./routes/main');
 const Detail = require('./models/Detail');
+const Slider = require('./models/Slider');
 const mongoose = require('mongoose');
 
 app.use("",routes);
@@ -17,6 +18,28 @@ hbs.registerPartials('views/partials');
 //db connection
 mongoose.connect('mongodb+srv://itswaleedtahir:itswaleedtahir123@cluster0.smf6v.mongodb.net/website_tut',()=>{
     console.log('Database connected');
+
+    // Slider.create([
+    //     {
+    //         title: "One of my favorite shot i took of nature",
+    //         subTitle:"The picture describes the beauty of SWAT ",
+    //         imageUrl:"/static/images/4.jpg"
+    //     },
+    //     {
+    //         title: "One of my favorite shot i took of nature",
+    //         subTitle:"The picture describes the beauty of SWAT ",
+    //         imageUrl:"/static/images/2.jpg"
+    //     },
+    //     {
+    //         title: "One of my favorite shot i took of nature",
+    //         subTitle:"The picture describes the beauty of SWAT ",
+    //         imageUrl:"/static/images/3.jpg"
+    //     },
+        
+    // ])
+
+
+
     // Detail.create({
     //     brandName: "Waleedz Photography",
     //     brandIconUrl:"https://yt3.ggpht.com/uBceQFYCeTQLHxcq8J1Ok9cT0HStGyr5t5UkXa8APs-rbysfBtpOMyLzBvixm5hkiFfYo5CvO90=s88-c-k-c0x00ffffff-no-rj",
