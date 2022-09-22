9db7dd7f37731b4d86eeb6162282db99d295b1d8
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2022-09-22 06:58:42 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
-console.log("working...");
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+const routes = require('./routes/main')
+const mongoose = require('mongoose');
+
+app.use("",routes);
+
+
+app.use('/static',express.static('public'));
+//templating engine
+app.set('view engine', 'hbs');
+app.set('views','views');
+
+//db connection
+mongoose.connect('mongodb://localhost/website_tut',()=>{
+    console.log('Database connected');
+})
+
+app.listen(process.env.PORT | 8000, ()=>{
+        console.log("listening on port 8000...");
+})
