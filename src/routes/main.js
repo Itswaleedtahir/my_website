d856aedd3dfffd21ddc064808c2d0aede27c4a79
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2022-09-22 11:30:56 +0500

    Update main.js

--- src/routes/main.js
+++ src/routes/main.js
@@ -2,18 +2,24 @@ const express = require("express");
 
 const routes = express();
 const Detail = require('../models/Detail');
+const Slider = require('../models/Slider');
 
 routes.get('/', async (req,res)=>{
 
     const details = await Detail.findOne({"_id":"632bdfac85cf55ca4dfcccf0"});
-    console.log(details);
+    const slides = await Slider.find();
+  //  console.log(slides);
    res.render('index',{
-        details: details
+        details: details,
+        slides: slides,
    })
 });
 
-routes.get('/gallery',(req,res)=>{
-    res.render('gallery');
+routes.get('/gallery',async (req,res)=>{
+    const details = await Detail.findOne({"_id":"632bdfac85cf55ca4dfcccf0"});
+    res.render('gallery',{
+        details: details
+    });
  });
 
 module.exports = routes;
