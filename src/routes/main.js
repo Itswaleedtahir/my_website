4ffb697447f8f56a1e880677129ebdabb650eb7d
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2022-09-22 09:36:23 +0500

    Update main.js

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,9 +1,15 @@
 const express = require("express");
 
 const routes = express();
+const Detail = require('../models/Detail');
 
-routes.get('/',(req,res)=>{
-   res.render('index');
+routes.get('/', async (req,res)=>{
+
+    const details = await Detail.findOne({"_id":"632bdfac85cf55ca4dfcccf0"});
+    console.log(details);
+   res.render('index',{
+        details: details
+   })
 });
 
 routes.get('/gallery',(req,res)=>{
